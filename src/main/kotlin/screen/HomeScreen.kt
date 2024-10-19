e76eb4fce781177839a1206047097664637c0822
sejun2 <qpfjf56@gmail.com>
2024-10-20 00:27:10 +0900

    fix view structure

---
+++ src/main/kotlin/screen/HomeScreen.kt
@@ -0,0 +1,14 @@
+package screen
+
+import viewmodel.MemoViewModel
+
+class HomeScreen(private val viewModel: MemoViewModel) : BaseMemoScreen() {
+    override fun displayView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun showOptions() {
+        TODO("Not yet implemented")
+    }
+
+}
