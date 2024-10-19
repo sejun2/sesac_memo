732963f3259d407d3c937283290d0a5489094080
sejun2 <qpfjf56@gmail.com>
2024-10-19 19:27:25 +0900

    add moemoviewmodel state

--- src/main/kotlin/viewmodel/MemoViewModel.kt
+++ src/main/kotlin/viewmodel/MemoViewModel.kt
@@ -1,5 +1,16 @@
 package viewmodel
 
+import model.Memo
+
 class MemoViewModel {
 
+    var uiState: MemoUIState = MemoUIState.HomeUIState
+    var memos: List<Memo> = mutableListOf()
+
+}
+
+
+sealed class MemoUIState {
+    object HomeUIState : MemoUIState()
+    // 기타 등등
 }
