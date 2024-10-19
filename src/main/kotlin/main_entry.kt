28d24cf15e1f3e2f846e1e29f23fd1afa7bf1f38
sejun2 <qpfjf56@gmail.com>
2024-10-19 22:42:50 +0900

    add uis

--- src/main/kotlin/main_entry.kt
+++ src/main/kotlin/main_entry.kt
@@ -1,27 +1,51 @@
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.selects.whileSelect
 import model.Memo
 import util.FileMemoDatabase
 import util.IMemoDatabase
+import viewmodel.MemoUIState
+import viewmodel.MemoViewModel
+import java.util.Scanner
 
 fun main(args: Array<String>) {
-   println("MAIN ENTRY POINT")
+    println("MAIN ENTRY POINT")
 
-   val memoDatabase: IMemoDatabase = FileMemoDatabase.getInstance()
+    val memoDatabase: IMemoDatabase = FileMemoDatabase.getInstance()
 
-   val res = memoDatabase.writeMemo(
-      listOf(
-         Memo(
-            id = 1, content = "A"
-         ),
-         Memo(
-            id = 2, content = "B"
-         ),
-         Memo(
-            id = 3, content = "C"
-         ),
-      )
-   )
+    val res = memoDatabase.writeMemo(
+        listOf(
+            Memo(
+                id = 1, content = "A"
+            ),
+            Memo(
+                id = 2, content = "B"
+            ),
+            Memo(
+                id = 3, content = "C"
+            ),
+        )
+    )
 
-//   val memos = memoDatabase.readMemo()
+    ConsoleView(viewModel = MemoViewModel()).start()
+}
 
-//   println(memos)
+class ConsoleView(private val viewModel: MemoViewModel) {
+    fun start() = runBlocking {
+        launch {
+            while (true) {
+                val state = viewModel.uiState.value
+                state.displayView()
+                state.showOptions()
+                print("Select an option: ")
+                val selection = readLine()?.toIntOrNull()
+                if (selection != null && selection > 0 && selection <= state.options.size) {
+                    viewModel.setUIState(state.options[selection - 1].uiState)
+                } else {
+                    println("Invalid selection. Please try again.")
+                }
+            }
+        }
+    }
 }
--- src/main/kotlin/viewmodel/MemoViewModel.kt
+++ src/main/kotlin/viewmodel/MemoViewModel.kt
@@ -1,16 +1,71 @@
 package viewmodel
 
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import model.Memo
 
 class MemoViewModel {
 
-    var uiState: MemoUIState = MemoUIState.HomeUIState
+    var uiState: MutableStateFlow<MemoUIState> = MutableStateFlow(
+        value = MemoUIState.HomeUIState
+    )
     var memos: List<Memo> = mutableListOf()
 
+    fun setUIState(uiState: MemoUIState) {
+        this.uiState.value = uiState
+    }
 }
 
 
 sealed class MemoUIState {
-    object HomeUIState : MemoUIState()
+    data object HomeUIState : MemoUIState() {
+        override fun displayView() {
+            println("homeUIState")
+        }
+
+        override fun showOptions() {
+            println(options.toString())
+        }
+
+        override val options: List<MemoOption>
+            get() = listOf(MemoOption.MODIFY, MemoOption.HELLO, MemoOption.WRITE)
+    }
+
+    data object ProfileUIState : MemoUIState() {
+        override fun displayView() {
+            println("profileUIState")
+        }
+
+        override fun showOptions() {
+            println(options.toString())
+        }
+
+        override val options: List<MemoOption>
+            get() = listOf(MemoOption.MODIFY, MemoOption.HELLO)
+    }
+
+    data object HelloUIState : MemoUIState() {
+        override fun displayView() {
+            println("helloUIState")
+        }
+
+        override fun showOptions() {
+            println(options.toString())
+        }
+
+        override val options: List<MemoOption>
+            get() = listOf(MemoOption.MODIFY)
+    }
     // 기타 등등
+
+    abstract fun displayView()
+    abstract fun showOptions()
+
+    abstract val options: List<MemoOption>
+}
+
+enum class MemoOption(val uiState: MemoUIState) {
+    MODIFY(uiState = MemoUIState.HomeUIState),
+    WRITE(uiState = MemoUIState.ProfileUIState),
+    HELLO(uiState = MemoUIState.HelloUIState),
 }
