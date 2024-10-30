aef31a50de827eb818947c05c765dde12adb5cba
Hah-nna <hj08hn@gmail.com>
2024-10-30 23:03:48 +0900

    edit InputView.kt
    - stringPrinter -> ConsoleIo로 이름 변경
    - printMessageAndOptions 오버로드(옵션만 출력)
    
    @Jeong Hana

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -2,11 +2,11 @@ package view
 
 import model.Option
 
-
-object stringPrinter {
+object ConsoleIo {
     fun printMessage(message: String) {
         println(message)
     }
+    fun input() = readLine()
 }
 
 
@@ -14,7 +14,7 @@ object stringPrinter {
 * @params: message: String, list: List<Option>
 * 우선 파라미터로 받은 메세지를 출력하고
 * 옵션들이 담긴 리스트를 mapIndexed로 순회하면서
-* 1(index + 1). option.optionName 2. option.optionName 3. option.optionName으로 출력하게 한다
+* 1(index + 1). option.name 2. option.name 3. option.name으로 출력하게 한다
 * 이 때 joinToString을 사용해 쉼표로 나누어주고 출력함
 * */
 fun<T> optionFormatter (list: List<Option<T>>): String {
@@ -26,17 +26,18 @@ fun<T> optionFormatter (list: List<Option<T>>): String {
 
 fun<T>printMessageAndOptions(list: List<Option<T>>, otherOptions: String) {
     val formattedOption = optionFormatter(list)
-    return stringPrinter.printMessage(formattedOption + otherOptions)
+    return ConsoleIo.printMessage(formattedOption + otherOptions)
 }
 
 fun<T>printMessageAndOptions(message: String, list: List<Option<T>>) {
     val formattedOption = optionFormatter(list)
-    return stringPrinter.printMessage(message + formattedOption)
+    return ConsoleIo.printMessage(message + formattedOption)
+}
+fun<T>printMessageAndOptions(list: List<Option<T>>) {
+    val formattedOption = optionFormatter(list)
+    return ConsoleIo.printMessage(formattedOption)
 }
 
-
-
-
 fun printMessageAndOptions(message: String, vararg options: String){
-    stringPrinter.printMessage(message.format(*options))
+    ConsoleIo.printMessage(message.format(*options))
 }
