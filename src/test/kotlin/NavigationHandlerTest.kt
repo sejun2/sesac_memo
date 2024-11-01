81997d00a7cbe4a325ddbb951c9d3b9ef5e04025
Hah-nna <hj08hn@gmail.com>
2024-11-02 00:42:09 +0900

    edit tests name
    - 테스트코드 이름을 변경하였습니다
    - 테스트 결과를 볼 때 좀 더 보기편하게 수정하였습니다
    
    @Jeong Hana

--- src/test/kotlin/NavigationHandlerTest.kt
+++ src/test/kotlin/NavigationHandlerTest.kt
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
 import screen.CategoryScreen
@@ -20,43 +21,49 @@ class NavigationHandlerTest {
         navigationHandler = NavigationHandler()
     }
 
-    @Test
+    @Nested
+    inner class SetScreenTest {
+        @Test
+        fun `setScreen change to newScreen` () {
+            val newScreen = CategoryScreen(HomeScreen(), true)
+            navigationHandler.setScreen(newScreen)
+            val currentScreen = navigationHandler.getCurrentScreen()
 
-    fun `setScreen change to newScreen` () {
-        val newScreen = CategoryScreen(HomeScreen(), true)
-        navigationHandler.setScreen(newScreen)
-        val currentScreen = navigationHandler.getCurrentScreen()
-
-        assertTrue(currentScreen is CategoryScreen, "currentScreen: $currentScreen, newScreen: $newScreen")
+            assertTrue(currentScreen is CategoryScreen, "currentScreen: $currentScreen, newScreen: $newScreen")
+        }
     }
 
-    @Test
-    fun `navigateToHomeScreen change currentScreen to HomeScreen` () {
-        navigationHandler.setScreen(CategoryScreen(HomeScreen(), true))
-        navigationHandler.navigateToHomeScreen()
-        val currentScreen = navigationHandler.getCurrentScreen()
 
-        assertTrue(currentScreen is HomeScreen, "currenScreen : $currentScreen")
+    @Nested
+    inner class NavigateToHomeScreenTest {
+        @Test
+        @DisplayName("currentScreen이 HomeScreen으로 변경됨")
+        fun `navigateToHomeScreen change currentScreen to HomeScreen` () {
+            navigationHandler.setScreen(CategoryScreen(HomeScreen(), true))
+            navigationHandler.navigateToHomeScreen()
+            val currentScreen = navigationHandler.getCurrentScreen()
+
+            assertTrue(currentScreen is HomeScreen, "currenScreen : $currentScreen")
+        }
     }
 
+@Nested
+inner class NavigateToDetailMemoScreen {
     @Test
     @DisplayName("navigateToDetailMemoScreen에 올바른 아이디값이 입력되었을 때 currentScreen값이 변경됨")
-    fun `if it's a valid id, navigateToDetailMemoScreen'll change currentScreen to DetailMemoScreen` () {
+    fun success () {
         navigationHandler.navigateToHomeScreen()
         navigationHandler.navigateToDetailMemoScreen(2)
         val currentScreen = navigationHandler.getCurrentScreen()
-        println(currentScreen)
         assertTrue(currentScreen is DetailMemoScreen, "currentScreen : $currentScreen")
     }
 
     @Test
-    @DisplayName("잘못된 아이디값(음수)이 입력되었을 때 currentScreen이 DetailMemoScreen이면 안 됨")
-
-    fun `if it's not a valid id, currentScreen'll not be changed to DetailMemoScreen1` () {
+    @DisplayName("navigateToDetailMemoScreen에 잘못된 아이디값(음수)이 입력되었을 때 currentScreen이 DetailMemoScreen이면 안 됨")
+    fun failA () {
         navigationHandler.setScreen(MemoListScreen())
         navigationHandler.navigateToDetailMemoScreen(-1)
         val currentScreen = navigationHandler.getCurrentScreen()
-        println("currentScreen: $currentScreen")
         assertAll(
             "if it's not a valid id, should not change currentScreen to DetailMemoScreen",
             { assertFalse(currentScreen is DetailMemoScreen, "DetailMemoScreen으로 변경되면 안 됨") },
@@ -65,20 +72,17 @@ class NavigationHandlerTest {
     }
 
     @Test
-    @DisplayName("잘못된 아이디값(memoList.size < id)이 입력되었을 때 currentScreen이 DetailMemoScreen이면 안 됨")
-
-    fun `if it's not a valid id, currentScreen'll not be changed to DetailMemoScreen2` () {
+    @DisplayName("navigateToDetailMemoScreen에 잘못된 아이디값(memoList.size < id)이 입력되었을 때 currentScreen이 DetailMemoScreen이면 안 됨")
+    fun failB () {
         val memoListSize = MemoViewModel.getInstance().fetchMemos().size + 1
         navigationHandler.setScreen(MemoListScreen())
         navigationHandler.navigateToDetailMemoScreen(memoListSize)
         val currentScreen = navigationHandler.getCurrentScreen()
-        println("currentScreen: $currentScreen")
         assertAll(
             "if it's not a valid id, should not change currentScreen to DetailMemoScreen",
             { assertFalse(currentScreen is DetailMemoScreen, "DetailMemoScreen으로 변경되면 안 됨") },
             { assertTrue(currentScreen is MemoListScreen, "currentScreen이 MemoList가 아님") }
         )
     }
-
-
+}
 }
