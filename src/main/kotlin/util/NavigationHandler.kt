e2444acdfcd47f26875af6ed630b1adb44671f2d
Hah-nna <hj08hn@gmail.com>
2024-11-05 15:40:37 +0900

    edit import NavigationHandler
    - NavigationHandler 중 navigateToDetailMemoScreen을 수정합니다
    
    @Jeong Hana

--- src/main/kotlin/util/NavigationHandler.kt
+++ src/main/kotlin/util/NavigationHandler.kt
@@ -3,7 +3,6 @@ package util
 import screen.BaseMemoScreen
 import screen.DetailMemoScreen
 import screen.HomeScreen
-import viewmodel.MemoViewModel
 
 class NavigationHandler() {
     private var currentScreen: BaseMemoScreen = HomeScreen()
@@ -19,11 +18,6 @@ class NavigationHandler() {
     }
 
     fun navigateToDetailMemoScreen(id: Int) {
-        val memoListLength = MemoViewModel.getInstance().fetchMemos().size
-        when{
-            id < 0 || id > memoListLength -> return
-            id in 1..memoListLength -> currentScreen = DetailMemoScreen(id)
-            else -> return
-        }
+        currentScreen = DetailMemoScreen(id)
     }
 }
