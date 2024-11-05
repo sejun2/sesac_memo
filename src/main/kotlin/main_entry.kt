a1709a3984ef7fa4f061a660b5a3fb669894a955
sejun2 <qpfjf56@gmail.com>
2024-11-05 10:54:14 +0900

    chore remove unused code

--- src/main/kotlin/main_entry.kt
+++ src/main/kotlin/main_entry.kt
@@ -8,25 +8,6 @@ import viewmodel.MemoViewModel
 fun main(args: Array<String>) {
     println("MAIN ENTRY POINT")
 
-    // below is only for test
-    val memoDatabase: IMemoDatabase = FileMemoDatabase.getInstance()
-
-    val res = memoDatabase.writeMemo(
-        listOf(
-            Memo(
-
-                id = 1, content = "A", Category.ETC
-            ),
-            Memo(
-                id = 2, content = "B", Category.TECH
-            ),
-            Memo(
-                id = 3, content = "C", Category.ECONOMY
-
-            ),
-        )
-    )
-
     ConsoleView(viewModel = MemoViewModel.getInstance()).start()
 }
 
